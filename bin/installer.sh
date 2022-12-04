64eb880a0e84482e641251ca421e2195f53a866b
Sam Su <samsu42@yahoo.ca>
2022-12-04 17:14:00 -0500

    Resolving POSIX compatibility and Shellcheck flagged issues

--- bin/installer.sh
+++ bin/installer.sh
@@ -30,15 +30,15 @@ REMOTE="https://github.com/Shougo/dein.vim.git"
 # 2. getent - to get the home path instead of ~ or pwd
 # 3. eval - to get home path in macOS
 USER=${USER:-$(id -u -n)}
-HOME=${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}
-HOME=${HOME:-$(eval echo ~$USER)}
+HOME=${HOME:-$(getent passwd "$USER" 2>/dev/null | cut -d: -f6)}
+HOME=${HOME:-$(eval echo ~"$USER")}
 
 # Improve user experience formatting messages with colors.
 # Usage: printf "$(ansi 32)"
 # 0: reset; 1: bold; 22: no bold; 31: red; 32: green; 33: yellow; 35: magenta; 36: cyan;
 ansi() {
   [ $# -gt 0 ] || return
-  printf "\033[%sm" $*
+  printf "\033[%sm" "$@"
 }
 # If stdout is not a terminal ignore all formatting
 [ -t 1 ] || ansi() { :; }
@@ -54,35 +54,35 @@ typography() {
   case $1 in
   "title")
     reset_clb
-    printf "$(ansi 1 32)_______________________________________________________________\n\n"
+    printf "%s_______________________________________________________________\n\n" "$(ansi 1 32)"
     printf " #######  ######## ### ###  ###       ###  ### ### ###########\n"
     printf " ##!  ### ##!      ##! ##!#!###       ##!  ### ##! ##! ##! ##!\n"
     printf " #!#  !#! #!!!:!   !!# #!##!!#!       #!#  !#! !!# #!! !#! #!#\n"
     printf " !!:  !!! !!:      !!: !!:  !!!        !:..:!  !!: !!:     !!:\n"
     printf " ::::::   :::::::: ::: :::   ::   ::     ::    ::: :::     :::\n"
-    printf "\n              $(ansi 0)by $AUTHOR $(ansi 1 32)•$(ansi 0) $LICENSE $(ansi 1 32)•$(ansi 0) v$VERSION$(ansi 1 32)\n"
-    printf "_______________________________________________________________$(ansi 0)\n\n\n"
+    printf "\n              %sby $AUTHOR %s•%s $LICENSE %s•%s v$VERSION%s\n" "$(ansi 0)" "$(ansi 1 32)" "$(ansi 0)" "$(ansi 1 32)" "$(ansi 0)" "$(ansi 1 32)"
+    printf "_______________________________________________________________%s\n\n\n" "$(ansi 0)"
     ;;
   "header")
     reset_clb
-    printf "\n\n$(ansi 1)[ $2 ]$(ansi 1 0)\n"
+    printf "\n\n%s[ $2 ] %s\n" "$(ansi 1)" "$(ansi 1 0)"
     ;;
   "end")
-    printf "$(ansi 36)➤$(ansi 0) Installation finished.$(ansi 0)\n"
-    printf "$(ansi 36)➤$(ansi 0) Run $(ansi 36)'cat $DEIN/doc/dein.txt'$(ansi 0) for more usage information.$(ansi 0)\n"
+    printf "%s➤%s Installation finished.%s\n" "$(ansi 36)" "$(ansi 0)" "$(ansi 0)"
+    printf "%s➤%s Run %s'cat $DEIN/doc/dein.txt'%s for more usage information.%s\n" "$(ansi 36)" "$(ansi 0)" "$(ansi 36)" "$(ansi 0)" "$(ansi 0)"
     ;;
-  "output") printf "$(ansi 32)\n$2\n$(ansi 0)\n" ;;
-  "input_opt") printf "$(ansi 1 35)$2$(ansi 0) $(ansi 36)$3$(ansi 0) $(ansi 0 34)$4$(ansi 0) $(ansi 1 35)$5$(ansi 0)\n" ;;
-  "input") printf "\n$(ansi 32)➤$(ansi 0) $2\n" ;;
-  "action") printf "$(ansi 36)➤$(ansi 0) $2 $(ansi 36)$3$(ansi 0)\n" ;;
-  "error") printf "$(ansi 31)Error: $2$(ansi 0)\n" ;;
-  "warning") printf "$(ansi 33)Warning: $2$(ansi 0)\n" ;;
+  "output") printf "%s\n$2\n%s\n" "$(ansi 32)" "$(ansi 0)" ;;
+  "input_opt") printf "%s$2%s %s$3%s %s$4%s %s$5%s\n" "$(ansi 1 35)" "$(ansi 0)" "$(ansi 36)" "$(ansi 0)" "$(ansi 0 34)" "$(ansi 0)" "$(ansi 1 35)" "$(ansi 0)" ;;
+  "input") printf "\n%s%s$2\n" "$(ansi 32)" "$(ansi 0)";;
+  "action") printf "%s%s $2 %s$3%s\n" "$(ansi 36)" "$(ansi 0)" "$(ansi 36)" "$(ansi 0)" ;;
+  "error") printf "%sError: $2%s\n" "$(ansi 31)" "$(ansi 0)" ;;
+  "warning") printf "%sWarning: $2%s\n" "$(ansi 33)" "$(ansi 0)";;
   *) printf "" ;;
   esac
 }
 
 # Make sure git is installed and is executable
-command -v git >/dev/null 2>&1 || {
+command -v git >>/dev/null 2>&1 || {
   typography error "Please install git or update your path to include the git executable! Exit error."
   exit 1
 }
@@ -141,7 +141,7 @@ config_prompt() {
   while typography header "CONFIG LOCATION" &&
     typography input_opt "1" "vim" "path" "(~/.vimrc)" &&
     typography input_opt "2" "neovim" "path" "(~/.config/nvim/init.vim)" &&
-    typography input "Select your editor config location (eg. 1 or 2)" && read -p "$(ansi 32)➤$(ansi 0) " OPT_CL; do
+    typography input "Select your editor config location (eg. 1 or 2)" && read -r OPT_CL; do
     case $OPT_CL in
     1)
       VIMRC="${HOME}/.vimrc"
@@ -161,7 +161,7 @@ base_prompt() {
     typography input_opt "1" "cache" "path" "(~/.cache/dein)" &&
     typography input_opt "2" "local" "path" "(~/.local/share/dein)" &&
     typography input "Select dein.vim location to clone with git (eg. 1 or 2)" &&
-    read -p "$(ansi 32)➤$(ansi 0) " OPT_DL; do
+    read -r OPT_DL; do
     case $OPT_DL in
     1)
       BASE="${HOME}/.cache/dein"
@@ -191,13 +191,13 @@ dein_setup() {
     git checkout -b "$BRANCH" "origin/$BRANCH" -q || {
     [ ! -d "$DEIN" ] || {
       cd -
-      rm -rf "$DEIN" &>/dev/null
+      rm -rf "$DEIN" >>/dev/null 2>&1
     }
     typography error "Git clone of dein.vim repo failed"
     exit 1
   }
 
-  command cd - &>/dev/null || {
+  command cd - >>/dev/null 2>&1 || {
     typography error "Failed to exit installation directory"
     exit 1
   }
@@ -217,7 +217,7 @@ editor_setup() {
 
   OUTDIR=${OUTDIR:-$VIMRC}
 
-  if command echo "$(generate_vimrc)" >$OUTDIR; then
+  if command echo "$(generate_vimrc)" >"$OUTDIR"; then
     typography action "Config file created successfully!" "($OUTDIR)"
   else
     typography error "Failed to generate vim config file. ($OUTDIR)\nMake sure the directory exists and you have access to it."
@@ -230,7 +230,7 @@ dein() {
   while [ $# -gt 0 ]; do
     case $1 in
     --overwrite-config | -oWC) KEEP_CONFIG=no ;;
-    ./* | /home/* | ~/*) BASE=$(echo $1) ;;
+    ./* | /home/* | ~/*) BASE=$(echo "$1") ;;
     *)
       typography error "Invalid '$1' command line argument given."
       exit 1
@@ -249,7 +249,7 @@ dein() {
 
   DEIN="${BASE}/repos/github.com/Shougo/dein.vim"
 
-  if [ -d $DEIN ]; then
+  if [ -d "$DEIN" ]; then
     typography warning "The DEIN folder already exists ($DEIN).\nYou'll need to move or remove it."
     exit 1
   fi
